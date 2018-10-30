bd1093b9f09b7218608a848e4fc663d8c9142b91
Siphiwe-Manda <siphiwemanda@gmail.com>
2018-10-30 12:47:20 +0000

    Achieved: markers have pop up window

--- src/App.js
+++ src/App.js
@@ -1,78 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
 import './index.css'
-import axios from 'axios'
+/*import axios from 'axios'*/
 import Map from './Comp/Map'
-
-const parks = [
-  {
-    Runnames: "South Manchester parkrun",
-    location: "platt fields park",
-    organisation: "parkrun",
-    distance: "5km",
-    day: "Saturday",
-    time: "9AM",
-    LatLng: {lat: 53.447456, lng: -2.224610}
-  },
-
-  {
-    Runnames: "Birchfields Park",
-    Parkname: "Birchfields Park",
-    organisation: "Great Local Run",
-    distance: "5km, 2km",
-    day: "Sunaday",
-    time: "11AM",
-    LatLng: {lat: 53.451355, lng: -2.212029}
-
-  },
-
-  {
-    Runnames: "Burnage parkrun",
-    location: "Varley Park",
-    organisation: "parkrun",
-    distance: "5km",
-    day: "Saturday",
-    time: "9AM",
-    LatLng: {lat: 53.428904, lng: -2.190995}
-
-  },
-  {
-    Runnames: "Salford Quays",
-    location: "Salford Quays, Manchester",
-    organisation: "Great Local Run",
-    distance: "5km, 2km",
-    day: "Wednessday",
-    time: "6:45PM",
-    LatLng: {lat: 53.471892, lng: -2.299572}
-
-  },
-
-  {
-    Runnames: "Debdale Park",
-    location: "Debdale Park, Manchester",
-    organisation: "Great Local Run",
-    distance: "5km, 2km",
-    day: "Sunday",
-    time: "9AM",
-    LatLng: {lat: 53.460548, lng: -2.154102}
-
-  },
-
-]
-
 
 
 class App extends Component {
 
+  state ={
+      parks: [
+      {
+        Runnames: "South Manchester parkrun",
+        location: "platt fields park",
+        organisation: "parkrun",
+        distance: "5km",
+        day: "Saturday",
+        time: "9AM",
+        LatLng: {lat: 53.447456, lng: -2.224610}
+      },
 
+      {
+        Runnames: "Birchfields Park",
+        Parkname: "Birchfields Park",
+        organisation: "Great Local Run",
+        distance: "5km, 2km",
+        day: "Sunaday",
+        time: "11AM",
+        LatLng: {lat: 53.451355, lng: -2.212029}
 
+      },
 
+      {
+        Runnames: "Burnage parkrun",
+        location: "Varley Park",
+        organisation: "parkrun",
+        distance: "5km",
+        day: "Saturday",
+        time: "9AM",
+        LatLng: {lat: 53.428904, lng: -2.190995}
 
+      },
+      {
+        Runnames: "Salford Quays",
+        location: "Salford Quays, Manchester",
+        organisation: "Great Local Run",
+        distance: "5km, 2km",
+        day: "Wednessday",
+        time: "6:45PM",
+        LatLng: {lat: 53.471892, lng: -2.299572}
+
+      },
+
+      {
+        Runnames: "Debdale Park",
+        location: "Debdale Park, Manchester",
+        organisation: "Great Local Run",
+        distance: "5km, 2km",
+        day: "Sunday",
+        time: "9AM",
+        LatLng: {lat: 53.460548, lng: -2.154102}
+
+      },
+
+    ]
+  }
   render() {
     return (
       <div className="App">
       <main>
-     <Map parks={parks}> </Map >
+     <Map parks={this.state.parks}> </Map >
     </main>
       </div>
     );
--- src/Comp/Map.js
+++ src/Comp/Map.js
@@ -26,8 +26,23 @@ initMap = () => {
           animation: window.google.maps.Animation.DROP,
           /*id: i,*/
         })
+       var largeInfowindow = new window.google.maps.InfoWindow();
+       marker.addListener('click', function() {
+          populateInfoWindow(this, largeInfowindow);
+        });
+        function populateInfoWindow(marker, infowindow) {
+              // Check to make sure the infowindow is not already opened on this marker.
+              if (infowindow.marker !== marker) {
+                infowindow.marker = marker;
+                infowindow.setContent('<div>' + marker.title + '</div>');
+                infowindow.open(Map, marker);
+                // Make sure the marker property is cleared if the infowindow is closed.
+                infowindow.addListener('closeclick',function(){
+                  infowindow.setMarker = null;
+                });
+              }
+            }
 
-  
 
       })
 console.log('beep')
