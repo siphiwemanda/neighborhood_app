541067c3e98cbdd3294c22e161d822d4d6b1b7ef
Siphiwe-Manda <siphiwemanda@gmail.com>
2018-11-01 18:16:44 +0000

    Achieved: went back to old map style

--- src/Comp/Map.js
+++ src/Comp/Map.js
@@ -1,44 +1,22 @@
 import React, { Component } from 'react';
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
-
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 class Map extends Component {
-
-  componentDidMount(){
-      this.renderMap()
-  }
-  renderMap =()=> {
-    loadscript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBndej0CC1LX31Kl_eo1JgkVz-BpWjVADo&callback=initMap")
-    window.initMap = this.initMap
-  }
-
-
-initMap = () => {
-    const Map = new window.google.maps.Map(document.getElementById('map'), {
-    center: {lat: 53.480759, lng: -2.242631},
-    zoom: 8
-    });
-}
-
-
-
-render(){
-  console.log('props', this.props)
-
-    return(
-      <div id='map'></div>
-    )
-  }
+   render() {
+   const GoogleMapExample = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter = { {lat: 53.480759, lng: -2.242631} }
+        defaultZoom = { 13 }
+      >
+      </GoogleMap>
+   ));
+   return(
+      <div>
+        <GoogleMapExample
+          containerElement={ <div style={{ height: `100vh`, width: '100vw' }} /> }
+          mapElement={ <div style={{ height: `100%` }} /> }
+        />
+      </div>
+   );
+   }
 };
-
-function loadscript(url){
-  var index = window.document.getElementsByTagName("script")[0]
-  var script =window.document.createElement("script")
-  script.src = url
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
-
-}
-
 export default Map;
